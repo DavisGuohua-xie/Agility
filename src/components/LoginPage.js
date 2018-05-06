22c299cba515571b11e0c0427cdaffb6460d6a31
Akshara Balachandra <abalacha@ucsd.edu>
2018-05-06 10:44:20 -0700

    Code indentations

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,18 +1,30 @@
 import React, {
     Component
 } from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import {withRouter} from 'react-router';
-import {authActions} from '../actions/authActions';
+
+import {
+    connect
+} from 'react-redux';
+
+import {
+    bindActionCreators
+} from 'redux';
+
+import {
+    withRouter
+} from 'react-router';
+
+import {
+    authActions
+} from '../actions/authActions';
 
 import LoginForm1 from './login/LoginForm1';
 
 export default class LoginPage extends Component {
-  
-  constructor(props) {
+
+    constructor(props) {
         super(props);
-    
+
         this.state = {
             username: "",
             password: "",
@@ -28,13 +40,13 @@ export default class LoginPage extends Component {
         this.handleToggleForgot = this.handleToggleForgot.bind(this);
         this.handleToggleReg = this.handleToggleReg.bind(this);
     }
-  
+
     handleLogin() {
         let username = this.state.username;
         let password = this.state.password;
 
         if (username && password) {
-          this.props.actions.login(username, password, this.props.history);
+            this.props.actions.login(username, password, this.props.history);
         }
     }
 
@@ -44,7 +56,7 @@ export default class LoginPage extends Component {
         let email = this.state.registerEmail;
 
         if (username && password && email) {
-          this.props.actions.register(username, password, email, this.props.history);
+            this.props.actions.register(username, password, email, this.props.history);
         }
     }
 
@@ -69,24 +81,40 @@ export default class LoginPage extends Component {
 
     render() {
         console.log(this.props.ajaxCalls);
-        return <LoginForm1 
-            onInputChange={this.handleInputChange}
-            onLogin={this.handleLogin}
-            onCreateAccount={this.handleCreateAccount}
-            onToggleForgot={this.handleToggleForgot}
-            isOpenForgot={this.state.isOpenForgot}
-            onToggleReg={this.handleToggleReg}
-            isOpenReg={this.state.isOpenReg}
-            ajaxRequested={this.props.ajaxCalls > 0}
-          />;
+        return <LoginForm1
+                    onInputChange = {
+                        this.handleInputChange
+                    }
+                    onLogin = {
+                        this.handleLogin
+                    }
+                    onCreateAccount = {
+                        this.handleCreateAccount
+                    }
+                    onToggleForgot = {
+                        this.handleToggleForgot
+                    }
+                    isOpenForgot = {
+                        this.state.isOpenForgot
+                    }
+                    onToggleReg = {
+                        this.handleToggleReg
+                    }
+                    isOpenReg = {
+                        this.state.isOpenReg
+                    }
+                    ajaxRequested = {
+                        this.props.ajaxCalls > 0
+                    }
+                />;
     }
 }
 
 
 function mapDispatchToProps(dispatch) {
-  return { 
-    actions: bindActionCreators(authActions, dispatch)
-  };
+    return {
+        actions: bindActionCreators(authActions, dispatch)
+    };
 }
 
 function mapStateToProps(state, ownProps) {
@@ -97,4 +125,6 @@ function mapStateToProps(state, ownProps) {
 }
 
 const connectedLoginPage = withRouter(connect(mapStateToProps, mapDispatchToProps)(LoginPage));
-export { connectedLoginPage as LoginPage };
+export {
+    connectedLoginPage as LoginPage
+};
