6dec0a36833f602e031ef1818af718788af7ebea
Akshara Balachandra <akshara.bala.18@gmail.com>
2018-05-16 18:10:25 -0700

    refactor: indentation in navbar.js

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,79 +1,79 @@
 import React from 'react';
 import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
+Collapse,
+Navbar,
+NavbarToggler,
+NavbarBrand,
+Nav,
+NavItem,
+UncontrolledDropdown,
+DropdownToggle,
+DropdownMenu,
+DropdownItem
 } from 'reactstrap';
 
 import {
-  Link
+Link
 } from 'react-router-dom';
 
 
 import styles from '../../styles/navbar.module.css';
 
 export default class NavBar extends React.Component {
-    constructor(props) {
-      super(props);
+  constructor(props) {
+    super(props);
 
-      this.toggle = this.toggle.bind(this);
-      this.state = {
-        isOpen: false
-      };
-    }
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
 
-    toggle() {
-      this.setState({
-        isOpen: !this.state.isOpen
-      });
-    }
-    
-    render() {
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  
+  render() {
 
-      let projName = this.props.projName ? this.props.projName : null;
-      console.log(projName);
-      return (
-        <div>
-          <Navbar color="dark" dark expand="md">
-            <Link to="/" className={`${styles.navbarBrand} navbar-brand`}>Agility</Link>
-            <NavbarToggler onClick={this.toggle} />
-            
-            <Nav className={styles.navbarCenter} navbar>
+    let projName = this.props.projName ? this.props.projName : null;
+    console.log(projName);
+    return (
+      <div>
+        <Navbar color="dark" dark expand="md"  style={{zIndex: 2}}>
+          <Link to="/" className={`${styles.navbarBrand} navbar-brand`}>Agility</Link>
+          <NavbarToggler onClick={this.toggle} />
+          
+          <Nav className={styles.navbarCenter} navbar>
+            {projName && <NavItem>
+              <Link to="/overview" className="nav-link">{projName}</Link>
+            </NavItem>}
+          </Nav>
+          
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar >
               {projName && <NavItem>
-                <Link to="/" className="nav-link">{projName}</Link>
+                <Link to="/chat" className="nav-link">Chat</Link>
               </NavItem>}
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Name
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem className={styles.dropdownItem}>
+                    <Link to="/myaccount" style={{textDecoration: 'none', color: '#212529'}}>My Account</Link>
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem className={styles.dropdownItem}>
+                    Logout
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
             </Nav>
-            
-            <Collapse isOpen={this.state.isOpen} navbar>
-              <Nav className="ml-auto" navbar>
-                {projName && <NavItem>
-                  <Link to="/chat" className="nav-link">Chat</Link>
-                </NavItem>}
-                <UncontrolledDropdown nav inNavbar>
-                  <DropdownToggle nav caret>
-                    Name
-                  </DropdownToggle>
-                  <DropdownMenu right>
-                    <DropdownItem className={styles.dropdownItem}>
-                      <Link to="/myaccount" style={{textDecoration: 'none', color: '#212529'}}>My Account</Link>
-                    </DropdownItem>
-                    <DropdownItem divider />
-                    <DropdownItem className={styles.dropdownItem}>
-                      Logout
-                    </DropdownItem>
-                  </DropdownMenu>
-                </UncontrolledDropdown>
-              </Nav>
-            </Collapse>
-          </Navbar>
-        </div>
-      );
-    }
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
