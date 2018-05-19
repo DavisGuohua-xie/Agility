aac9a2d977a90b7735bcee324a2cb04239fd6e2e
John Ooi <pwnerjohn@gmail.com>
2018-05-18 19:29:31 -0700

    feat: WIP new project modal

--- src/components/home/ProjectListComponent.js
+++ src/components/home/ProjectListComponent.js
@@ -2,22 +2,67 @@ import React from 'react';
 
 
 import ProjectList from './ProjectList';
+import { Button, Form, FormGroup, Input, Label, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 export default class ProjectListComponent extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            projItems: props.projects
+            projItems: props.projects,
+            newProjectModalOpen: false,
+            newProjectName: '',
+            newProjectMembers: '' // TODO: handle adding new project members to project on creation
         }
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleCreateProject = this.handleCreateProject.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+
+
+    toggleModal() {
+        this.setState({newProjectModalOpen: !this.state.newProjectModalOpen});
+    }
+
+    handleCreateProject() {
+        // todo: projActions
+        console.log("project name: " + this.state.newProjectName);
+        console.log("TODO: create project")
+    }
+
+    handleNameChange(e) {
+        this.setState({newProjectName: e.target.value});
     }
 
     render() {
-        return <ProjectList 
+
+        return <div>
+                <Modal isOpen={this.state.newProjectModalOpen} toggle={this.toggleModal} className={this.props.className}>
+                    <ModalHeader toggle={this.toggleModal}> Create New Project </ModalHeader>
+                    <ModalBody>
+                        <Form>
+                            <FormGroup>
+                                <Label for="projectName">Project Name</Label>
+                                <Input type="text" name="projectnameinput" id="projectName" placeholder="Agility" onChange={this.handleNameChange}/> 
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="addMembers">Add Members</Label>
+                                <Input type="text" name="addmembersinput" id="addMembers" placeholder="TODO: functionality not yet implemented" /> 
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.handleCreateProject}>Create Project</Button>
+                        <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+                <ProjectList 
                     projects={this.state.projItems}
-                    onNewProject={this.handleNewProject}
+                    onNewProject={this.toggleModal}
                     onEnterProject={this.handleEnterProject}
-                />;
+                />
+                </div>;
     }
 
 };
