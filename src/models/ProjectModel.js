31b72e1bf52b63122ab23816306e88738871bb1d
Eduardo Carrillo <edcarril@ucsd.edu>
2018-05-19 21:00:55 -0700

    Changed let -> var for array reads. Added read and write functionality for Project Model

--- src/models/ProjectModel.js
+++ src/models/ProjectModel.js
@@ -1,14 +1,29 @@
 import Parse from 'parse'
 
-
-
-
-class ProjectModel extends Parse.Object {
+export class ProjectModel extends Parse.Object {
 
      static let PROJECT_MANAGER = "ProjectManager";
      static let TEAM_MEMBER = "TeamMember";
      static let CUSTOMER = "Customer";
      static let CEO = 'CEO';
+
+
+     static let ROLES = 'roles'
+     static let TASKS = 'tasks'
+     static let CHANNELS = 'channels'
+     static let UPDATES = 'updates'
+     static let BOARDS = 'boards'
+     static let NAME = 'name'
+
+
+     var defaultSuccessHandler = function(data){
+       console.log('Succesfully got back data')
+     }
+
+     var defaultErrorHandler = function(data, error){
+       console.log('data: ' + data + 'Could not be saved! \n')
+       console.log('[ERROR]', error)
+     }
 
 
   className(){
@@ -20,69 +35,145 @@ class ProjectModel extends Parse.Object {
        //Other initialization
 
        this.roles = {} // Empty dictionary
+       this.set(ProjectModel.ROLES, this.roles)
        this.tasks = [] //Empty array of tasks
+       this.set(ProjectModel.TASKS, this.tasks)
+
        this.channels = [] //Empty array of channels
+       this.set(ProjectModel.CHANNELS, this.channels)
+
        this.updates = [] //Empty array of updates objects
+       this.set(ProjectModel.UPDATES, this.updates)
+
        this.boards  = [] //Empty array of boards
+       this.set(ProjectModel.BOARDS, this.boards)
+
+
        this.name = "New Project"
+       this.set(ProjectModel.NAME, this.name)
 
      }
 
+     getRoles(){
+      return this.get(ProjectModel.ROLES)
+     }
 
-//TODO add role
-    addRole(user, role=TEAM_MEMBER){
-      this.roles[user] = role;
-    }
-//TODO add new task
-    addNewTask(){
 
-    }
-
-//// TODO: Remove task for index
-    removeTaskWithIndex(index){
-
-    }
-//TODO: Remove task with task
-    removeTaskWithTask(task){
-
+    getTasks(){
+      return this.get(ProjectModel.TASKS)
     }
 
-
-   //TODO: Create a new channel to project
-    addNewChannel(){
-
+    getChannels(){
+      return this.get(ProjectModel.CHANNELS)
     }
 
-    //TODO: Create new update for the projects
-    addNewUpdate(){
+    getUpdates(){
+      return this.get(ProjectModel.UPDATES)
+    }
 
+    getBoards(){
+      return this.get(ProjectModel.BOARDS)
+    }
+
+    getName(){
+      return this.get(ProjectModel.NAME)
     }
 
 
-    //TODO: Add a new board to the project
-    addBoard(){
+    addRole(user, role, successHandler, errorHandler){
+      var roles = this.get(ProjectModel.ROLES)
+      roles[user.id] = role;
+      this.set(ProjectModel.ROLES, roles)
 
-    }
-
-    //TODO: Remove board from the projects
-    removeBoard(){
-
-    }
-
-
-    renameProject(newName){
-      this.name = newName
+      saveData(this,successHandler, errorHandler)
     }
 
 
+    removeUserFromRole(user, successHandler, errorHandler){
+      var roles = this.get(ProjectModel.ROLES)
+
+      delete roles[user.id]
+
+      this.set(ProjectModel.ROLES, roles);
+
+      saveData(this, successHandler, errorHandler)
+    }
+
+
+    addNewChannel(channel, successHandler, errorHandler){
+      var channels = this.get(ProjectModel.CHANNELS)
+
+      channels.push(channel)
+
+      this.set(ProjectModel.CHANNELS)
+      saveDate(this, successHandler, errorHandler)
+    }
 
 
 
+    addNewUpdate(update, successHandler, errorHandler){
+      var updates = this.get(ProjectModel.UPDATES)
+
+      updates.push(update)
+
+      this.set(ProjectModel.UPDATES, updates)
+
+      saveData(this, successHandler, errorHandler)
+    }
 
 
+    addBoard(board, successHandler, errorHandler){
+      var boards = this.get(ProjectModel.BOARDS)
+      boards.push(board)
+
+      this.set(ProjectModel.BOARDS, boards)
+
+      saveData(this, successHandler, errorHandler)
+
+    }
+
+    removeBoardByIndex(index, successHandler, errorHandler){
+      var boards = this.get(ProjectModel.BOARDS)
+      boards.splice(index, 0)
+
+      this.set(ProjectModel.BOARDS, boards)
+
+      saveData(this, successHandler, errorHandler)
+    }
+
+    removeBoardByBoard(board, successHandler, errorHandler){
+
+      var boards = this.get(ProjectModel.BOARDS)
+      boards.filter(function(currBoard){
+        return currBoard.id != board.id
+      })
+
+      this.set(ProjectModel.BOARDS, boards)
+
+      saveData(this, successHandler, errorHandler)
+
+    }
 
 
+    renameProject(newName, successHandler, errorHandler){
 
+      this.set(ProjectModel.NAME, newName)
+      saveData(this, successHandler, errorHandler)
+    }
 
+    saveData(pfobject, successHandler, errorHandler){
+     if (successHandler == null || successHandler == undefined) {
+       successHandler = this.defaultSuccessHandler
+     }
+
+     if (errorHandler == null || errorHandler == undefined){
+       errorHandler = this.defaultErrorHandler
+     }
+
+     pfobject.save(null, {
+       success: successHandler,
+       error: errorHandler
+     })
+    }
 
 }
