fc3ada1b8ac2a6d9fbfd0138e5a498cf9e7507b0
Eduardo Carrillo <edcarril@ucsd.edu>
2018-05-19 18:14:47 -0700

    Added skeleton for channel

---
+++ src/models/ChannelModel.js
@@ -0,0 +1,43 @@
+import Parse from 'parse'
+
+
+
+class Channel extends Parse.Object {
+
+    className (){
+      return 'Channel'
+    }
+
+
+    constructor(){
+      super(className())
+
+
+      this.isChannel = false
+      this.participants = []
+      this.name = 'NewChannel'
+      this.history = [] // Array of maps
+    }
+
+
+//// TODO: Implement
+    setIsChannel(isGroup){
+      this.isChannel = isGroup
+    }
+//TODO: Implement
+    addParticipant(user){
+      this.participants.append(user)
+    }
+
+    updateName(newName){
+      this.name = newName
+    }
+
+
+    addToHistory(message){
+      this.history.append(message)
+    }
+
+
+
+}
