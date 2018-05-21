a39de57cf63b5c16071ac615cc580274a003a5b2
John Ooi <pwnerjohn@gmail.com>
2018-05-20 23:01:10 -0700

    refactor: create new project concurrency and follows db schema reqs.

--- src/actions/projActions.js
+++ src/actions/projActions.js
@@ -13,50 +13,88 @@ function createProject(projectName, projectMembers, history) {
     let Project = Parse.Object.extend("Project");
     let project = new Project();
 
-    let roles = {};
     project.set("name", projectName);
     project.set("boards", []);
     project.set("channels", []);
     project.set("tasks", []);
     project.set("updates", []);
 
-    roles[projectMembers[0].id] = "ProjectManager";
-    projectMembers.shift();
+    project.save(null, {
+      success: function(project) {
+        saveMembersToProject(project, projectMembers, history);
+        dispatch(success(project));
+      },
+      error: function(project, error) {
+        dispatch(failure(error));
+        console.log(error);
+      }
+    });
+  };
 
-    getMembers(projectMembers).then(res => {
+  function request(req) {
+    return { type: types.CREATE_PROJECT_REQUEST, req };
+  }
+  function success(req) {
+    return { type: types.CREATE_PROJECT_SUCCESS, req };
+  }
+  function failure(req) {
+    return { type: types.CREATE_PROJECT_FAILURE, req };
+  }
+}
+
+function saveMembersToProject(project, projectMembers, history) {
+  let roles = {};
+
+  let promises = []
+  getMembers(projectMembers)
+    .then(res => {
       res.forEach(user => {
-        console.log(user.length);
+        // if user exists
         if (user.length !== 0) {
-            if (!(user[0].id in roles)) {
-                roles[user[0].id] = "ProjectMember";
+          // ignore duplicate roles
+          if (!(user[0].id in roles)) {
+            // if usernames match, then this is the creator/project manager
+            if (user[0].get("username") === projectMembers[0]) {
+              roles[user[0].id] = "ProjectManager";
+            } else {
+              roles[user[0].id] = "ProjectMember";
             }
+
+            // if user is not in any projects, we need to create the projects array
+            if (!user[0].get("projects")) {
+              user[0].set("projects", [project]);
+            } else {
+              user[0].add("projects", project);
+            }
+
+            // save when done with user modifications.
+            promises.push(user[0].save(null, {
+              useMasterKey: true,
+              success: function(res) {
+                console.log("successfully added project to user...");
+              },
+              error: function(res, err) {
+                console.log("error adding project to user: " + err);
+              }
+            }));
+          }
         }
       });
 
+      return Promise.all(promises);
+    })
+    .then(whatever => {
+      // save project roles
       project.set("roles", roles);
-
       project.save(null, {
         success: function(project) {
-          dispatch(success(project));
           history.push("/" + project.id + "/overview");
         },
         error: function(project, error) {
-          dispatch(failure(error));
-          console.log(error);
+          console.log("error in saving project roles! " + error);
         }
       });
     });
-
-    function request(req) {
-      return { type: types.CREATE_PROJECT_REQUEST, req };
-    }
-    function success(req) {
-      return { type: types.CREATE_PROJECT_SUCCESS, req };
-    }
-    function failure(req) {
-      return { type: types.CREATE_PROJECT_FAILURE, req };
-    }
-  };
 }
 
 function getMembers(projectMembers) {
@@ -70,6 +108,5 @@ function getMembers(projectMembers) {
     promises.push(query.find());
   }
 
-  console.log(promises);
   return Promise.all(promises);
 }
