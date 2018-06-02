e55ab91e7a1817004b2e840b2a16b67dbbc0543b
Shengkai He <hskthe1@gmail.com>
2018-06-02 13:55:09 -0700

    memberActions WIP

--- src/actions/memberActions.js
+++ src/actions/memberActions.js
@@ -8,10 +8,47 @@ export const memberActions = {
     removeMember
 };
 
-function addMember () {
-    
+function addMember (username, project_id, user_role) {
+    return dispatch => {
+        // call create member request
+        dispatch(request());
+        let query = Parse.Query(Parse.User);
+        query.equalTo("username", username);
+
+        query.first().then(user => {
+            let query = Parse.Query(Parse.Object.extend("Project"));
+
+            query.equalTo("objectId", project_id);
+
+            query.first().then(project => {
+                user.add("projects", project_id);
+                let roles = project.get("roles");
+
+                roles[username] = user_role;
+
+                project.set("roles", roles);
+                project.save().then( res => {
+                    // you finished saving, call dispatch success
+                }).catch( error => {
+                    // something went wrong, call dispatch failure
+                })
+            })
+        })
+    }
+
+    function request() {
+        return { type: types.CREATE_PROJECT_REQUEST };
+    }
+    function success(req) {
+        return { type: types.CREATE_PROJECT_SUCCESS, req };
+    }
+    function failure(req) {
+        return { type: types.CREATE_PROJECT_FAILURE, req };
+    }
 }
 
 function removeMember () {
+    return dispatch => {
 
+    }
 }
