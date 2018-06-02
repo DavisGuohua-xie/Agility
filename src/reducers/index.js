3df643ac0a6c229f2dd5b3311b1c6677ee6df4d5
John Ooi <pwnerjohn@gmail.com>
2018-06-02 01:10:56 -0700

    feat/bugfix/refactor: project overview, board overview, create new boards, remove extra history

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import ajaxCallsInProgress from "./ajaxStatusReducer";
 import authReducer from "./authReducer";
 import projectReducer from "./projectReducer";
+import taskReducer from "./taskReducer";
 
 const rootReducer = combineReducers({
     ajaxCallsInProgress,
     authReducer,
-    projectReducer
+    projectReducer,
+    taskReducer
 });
 
 export default rootReducer;
--- src/reducers/projectReducer.js
+++ src/reducers/projectReducer.js
@@ -6,18 +6,35 @@ export default function projectReducer(state = initialState, action) {
     switch (action.type) {
         case types.GET_PROJECT_LIST_REQUEST:
             return state.merge({
-                project_request: true
+                project_list_request: true
             });
 
         case types.GET_PROJECT_LIST_SUCCESS:
             return state.merge({
-                project_request: false,
+                project_list_request: false,
                 projects: action.projects
             });
 
         case types.GET_PROJECT_LIST_FAILURE:
             return state.merge({
-                project_request: false
+                project_list_request: false
+            });
+
+        case types.GET_PROJECT_REQUEST:
+            return state.merge({
+                project_request: true
+            });
+
+        case types.GET_PROJECT_SUCCESS:
+            return state.merge({
+                project_request: false,
+                project_data: action.project_data
+            });
+
+        case types.GET_PROJECT_FAILURE:
+            return state.merge({
+                project_request: false,
+                project_error: action.error
             });
 
         default:
