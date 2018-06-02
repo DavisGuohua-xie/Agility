1e8f8e74703c985cd73d1bf1049b85c36d0595de
Eduardo Carrillo <edcarril@ucsd.edu>
2018-06-02 09:21:15 -0700

    Added logging middleware for dispatch action degbugging

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -2,6 +2,22 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers";
 import thunk from "redux-thunk";
 
+
+const consoleMessages = store => next => action => {
+  let result
+  console.groupCollapsed(`dispatching action ${action.type}`)
+  console.log('BEFORE: ', store.getState())
+
+  result = next(action)
+
+  console.log('AFTER: ', store.getState())
+
+  console.groupEnd();
+
+
+  return result;
+}
+
 export default function configureStore(initialState) {
-    return createStore(rootReducer, initialState, applyMiddleware(thunk));
+    return applyMiddleware(thunk,consoleMessages)(createStore)(rootReducer,initialState);
 }
