97b9bc4a1ea08741008489e9c577e05de62320f1
Eduardo Carrillo <edcarril@ucsd.edu>
2018-06-02 09:29:27 -0700

    Added user model to proj list comp

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -3,5 +3,8 @@ import Immutable from "seamless-immutable";
 export default Immutable({
     ajaxCallsInProgress: 0,
     logging_in: false,
-    logged_in: false
+    logged_in: false,
+    user: null,
+    errors: [],
+    loggedIn: false
 });
