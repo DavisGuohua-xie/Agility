c6830c6fa9879ef629cc7b482a036064bb87a026
Shengkai <hskthe1@gmail.com>
2018-06-01 23:29:50 -0700

    added redux boilerplates

---
+++ src/actions/memberActions.js
@@ -0,0 +1,12 @@
+import * as types from "./actionTypes";
+import Parse from "parse";
+import * as ajaxActions from './ajaxActions';
+import history from '../history'
+
+export function addMember () {
+
+}
+
+export function removeMember () {
+    
+}
